5b4e8ecc75286517c787006c224abb222789ff40
Elena <elenacecilliaespinoza@gmail.com>
2024-12-20 12:52:40 -0300

    Primera versión de BadiePerico_app

---
+++ BadiePerico_app.py
@@ -0,0 +1,8 @@
+# -- coding: utf-8 --
+import streamlit as st
+
+# Título de la aplicación
+st.title("Hola, mundo")
+
+# Descripción o contenido de la aplicación
+st.write("Esta es mi primera aplicación con Streamlit")
